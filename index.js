230f9c2d3ffe0d7131ee8dcf084f6211849c1e57
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-04-26 20:08:18 -0700

    commit tree 75e92bdfa30884f3deeba43f3c14c80834605cf8

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
+let num1 =2;
+let num2 =31;
+let num3 = 14;
+let num4 = 5;
+const multiply = num1*num2;
 // Write your code here
+
+const random = Math.floor(Math.random()* 10);
+
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,20);
